83bcbf04e3e6eaa875ef62ba8b715df6f5a96960
Shawn Shacterman <shawnshact@gmail.com>
2020-12-13 15:08:31 -0800

    initial commit for jsma attack (not working)

--- pymarl/src/controllers/basic_controller.py
+++ pymarl/src/controllers/basic_controller.py
@@ -1,6 +1,8 @@
 from modules.agents import REGISTRY as agent_REGISTRY
 from components.action_selectors import REGISTRY as action_REGISTRY
+import math
 import torch as th
+from torch.autograd.gradcheck import zero_gradients
 
 
 # This multi-agent controller shares parameters between agents
@@ -13,24 +15,65 @@ class BasicMAC:
         self.agent_output_type = args.agent_output_type
 
         self.action_selector = action_REGISTRY[args.action_selector](args)
+        self.hidden_states = None 
 
-        self.hidden_states = None
+
+    def jsma(self, inputs, target_class, agent_idx = 0, max_distortion=0.1):
+
+        # Make a clone since we will alter the values
+        input_features = th.autograd.Variable(inputs.clone(), requires_grad=True)
+        input_features.requires_grad = True
+
+        num_features = input_features.size(1)
+        hidden_states = self.hidden_states.view(-1, self.n_agents,self.hidden_states.shape[-1])[:,agent_idx]
+        output, _ = self.agent(input_features, hidden_states)
+        source_class = th.argmax(output[0])
+        theta = 0.1
+        max_theta = 0.9 # time running should not depend on this
+        k = 0
+        max_iter = 20
+        while source_class != target_class and k < max_iter:
+            jacobian = self.compute_jacobian(input_features, output)
+            i,j,d = self.select_best_saliency_map(jacobian, num_features, target_class)
+
+            perturbation = theta*d
+            input_features.data[0,i] += perturbation
+            input_features.data[0,j] += perturbation
+
+            output, _ = self.agent(input_features, hidden_states)
+            source_class = th.argmax(output[0])
+            theta = min(theta+.1,max_theta)
+            k+=1
+        print(input_features.shape)
+        return input_features
+ 
 
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
         # Only select actions for the selected batch elements in bs
         avail_actions = ep_batch["avail_actions"][:, t_ep]
-        agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        
+        agent_idx, target_class = 0, 2
+        agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode, adversarial=True, agent_idx=agent_idx, target_class=target_class)
+
+        chosen_class = th.argmax(agent_outputs[0][agent_idx])
+
+        print("Target: {}, Chosen: {}\n".format(target_class, chosen_class))
+
         chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
         return chosen_actions
 
-    def forward(self, ep_batch, t, test_mode=False):
+    def forward(self, ep_batch, t, test_mode=False, adversarial=False, agent_idx=0, target_class=0):
         agent_inputs = self._build_inputs(ep_batch, t)
+        if adversarial:
+            agent_inputs = agent_inputs.view(ep_batch.batch_size, self.n_agents,-1)
+            victim_input = self.jsma(agent_inputs[:,agent_idx], target_class=target_class, agent_idx=agent_idx)
+            agent_inputs[:,agent_idx] = victim_input
+            agent_inputs = agent_inputs.view(ep_batch.batch_size*self.n_agents, -1)
+
         avail_actions = ep_batch["avail_actions"][:, t]
         agent_outs, self.hidden_states = self.agent(agent_inputs, self.hidden_states)
-
-        # Softmax the agent outputs if they're policy logits, only for COMA
+        # Softmax the agent outputs if they're policy logits
         if self.agent_output_type == "pi_logits":
-
             if getattr(self.args, "mask_before_softmax", True):
                 # Make the logits for unavailable actions very negative to minimise their affect on the softmax
                 reshaped_avail_actions = avail_actions.reshape(ep_batch.batch_size * self.n_agents, -1)
@@ -69,7 +112,7 @@ class BasicMAC:
         th.save(self.agent.state_dict(), "{}/agent.th".format(path))
 
     def load_models(self, path):
-        self.agent.load_state_dict(th.load("{}/agent.th".format(path)))
+        self.agent.load_state_dict(th.load("{}/agent.th".format(path), map_location=lambda storage, loc: storage))
 
     def _build_agents(self, input_shape):
         self.agent = agent_REGISTRY[self.args.agent](input_shape, self.args)
@@ -99,3 +142,61 @@ class BasicMAC:
             input_shape += self.n_agents
 
         return input_shape
+
+    def compute_jacobian(self, inputs, output):
+        """
+        :param inputs: Batch X Size (e.g. Depth X Width X Height)
+        :param output: Batch X Classes
+        :return: jacobian: Batch X Classes X Size
+        """
+        assert inputs.requires_grad
+
+        num_classes = output.size()[1]
+
+        jacobian = th.zeros(num_classes, *inputs.size())
+        grad_output = th.zeros(*output.size())
+        if inputs.is_cuda:
+            grad_output = grad_output.cuda()
+            jacobian = jacobian.cuda()
+
+        for i in range(num_classes):
+            zero_gradients(inputs)
+            grad_output.zero_()
+            grad_output[:, i] = 1
+            output.backward(grad_output, retain_graph=True)
+            jacobian[i] = inputs.grad.data
+
+        return th.transpose(jacobian, dim0=0, dim1=1)
+
+
+    def select_best_saliency_map(self, jacobian, num_features, target_index):
+        jacobian = jacobian.squeeze(0)
+        all_sum = th.sum(jacobian, 0) # sum all q partial derivatives for each feature
+        alpha = jacobian[target_index] # get target q partial derivative for each feature
+        
+        best_score = best_f1 = best_f2 = 0
+        best_i = best_j = 0
+        for i in range(num_features):
+            for j in range(i+1, num_features):
+                
+                f1 = alpha[i] # dQ_t/dx_i
+                f2 = alpha[j] # dQ_t/dx_j
+                
+                remaining_sum = all_sum[i]+all_sum[j]-f1-f2 # sum_k(dQ_k/dx_i) + sum_k(dQ_k/dx_j) - dQ_t/dx_i - dQ_t/dx_j
+                saliency_score = (f1+f2)*remaining_sum # (dQ_t/dx_i+dQ_t/dx_j)*sum_(k != t)(dQ_k/dx_i+dQ_k/dx_j)
+
+                if saliency_score > 0:
+                    saliency_score = 0
+                else:
+                    saliency_score *= -1
+
+                if saliency_score > best_score:
+                    best_score = saliency_score
+                    best_f1, best_f2 = f1, f2
+                    best_i, best_j = i, j
+
+        return best_i,best_j, math.copysign(1, best_f1+best_f2)
+                
+
+
+
